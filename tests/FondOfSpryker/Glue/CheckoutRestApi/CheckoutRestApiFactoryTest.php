79a22e778fd0a1c83280b1f1a82398e3602ec46c
Markus Nörenberg <markus.noerenberg@fondofbags.com>
2019-05-14 10:30:57 +0200

    add some test

---
+++ tests/FondOfSpryker/Glue/CheckoutRestApi/CheckoutRestApiFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FondOfSpryker\Glue\CheckoutRestApi;
+
+use Codeception\Test\Unit;
+
+class CheckoutRestApiFactoryTest extends Unit
+{
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateCustomerGroupsResourceRelationshipExpander(): void
+    {
+
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateCustomerGroupsMapper(): void
+    {
+
+    }
+}
